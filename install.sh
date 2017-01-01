68569755665750c997df4d80f3fb4865d73632ba
Zdenek Kostal <zdenek@toptal.com>
2017-01-01 21:36:19 +0100

    Migrate to nvim's config structure

--- install.sh
+++ install.sh
@@ -7,26 +7,67 @@
 
 ########## Variables
 
-dir=~/dotfiles                    # dotfiles directory
-olddir=~/dotfiles_old             # old dotfiles backup directory
-files="vimrc zshrc fzf.zsh gitconfig tmux.conf tmux.layout tmux.style tigrc"    # list of files/folders to symlink in homedir
+DOTFILES=~/dotfiles                    # dotfiles directory
+OLD_DOTFILES=~/dotfiles_old             # old dotfiles backup directory
+files="zshrc fzf.zsh gitconfig tmux.conf tmux.layout tmux.style tigrc"    # list of files/folders to symlink in homedir
 
 ##########
 
 # create dotfiles_old in homedir
-echo "Creating $olddir for backup of any existing dotfiles in ~"
-mkdir -p $olddir
+echo "Creating $OLD_DOTFILES for backup of any existing dotfiles in ~"
+mkdir -p $OLD_DOTFILES
 echo "...done"
 
 # change to the dotfiles directory
-echo "Changing to the $dir directory"
-cd $dir
+echo "Changing to the $DOTFILES directory"
+cd $DOTFILES
 echo "...done"
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks
 for file in $files; do
-    echo "Moving any existing dotfiles from ~ to $olddir"
+    echo "Moving any existing dotfiles from ~ to $OLD_DOTFILES"
     mv ~/.$file ~/dotfiles_old/
     echo "Creating symlink to $file in home directory."
-    ln -s $dir/$file ~/.$file
+    ln -s $DOTFILES/$file ~/.$file
+done
+
+# https://github.com/nicknisi/dotfiles
+
+echo -e "\n\nInstalling to ~/.config"
+echo "=============================="
+if [ ! -d $HOME/.config ]; then
+    echo "Creating ~/.config"
+    mkdir -p $HOME/.config
+fi
+for config in $DOTFILES/config/*; do
+    target=$HOME/.config/$( basename $config )
+    if [ -e $target ]; then
+        echo "~${target#$HOME} already exists... Skipping."
+    else
+        echo "Creating symlink for $config"
+        ln -s $config $target
+    fi
+done
+
+# create vim symlinks
+# As I have moved off of vim as my full time editor in favor of neovim,
+# I feel it doesn't make sense to leave my vimrc intact in the dotfiles repo
+# as it is not really being actively maintained. However, I would still
+# like to configure vim, so lets symlink ~/.vimrc and ~/.vim over to their
+# neovim equivalent.
+
+echo -e "\n\nCreating vim symlinks"
+echo "=============================="
+VIMFILES=( "$HOME/.vim:$DOTFILES/config/nvim"
+        "$HOME/.vimrc:$DOTFILES/config/nvim/init.vim" )
+
+for file in "${VIMFILES[@]}" ; do
+    KEY=${file%%:*}
+    VALUE=${file#*:}
+    if [ -e ${KEY} ]; then
+        echo "${KEY} already exists... skipping"
+    else
+        echo "Creating symlink for $KEY"
+        ln -s ${VALUE} ${KEY}
+    fi
 done
