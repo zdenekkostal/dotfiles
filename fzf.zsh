d9a59de53549526095b1e13dbbd4cc5e3ca3a547
Zdenek Kostal <zdenek@toptal.com>
2017-01-01 19:15:28 +0100

    Update fzf script

--- fzf.zsh
+++ fzf.zsh
@@ -4,12 +4,6 @@ if [[ ! "$PATH" == */Users/z/.fzf/bin* ]]; then
   export PATH="$PATH:/Users/z/.fzf/bin"
 fi
 
-# Man path
-# --------
-if [[ ! "$MANPATH" == */Users/z/.fzf/man* && -d "/Users/z/.fzf/man" ]]; then
-  export MANPATH="$MANPATH:/Users/z/.fzf/man"
-fi
-
 # Auto-completion
 # ---------------
 [[ $- == *i* ]] && source "/Users/z/.fzf/shell/completion.zsh" 2> /dev/null
