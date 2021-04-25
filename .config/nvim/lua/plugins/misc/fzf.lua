d497005e77af4c29caf2dc3c1f0bb12e5d7d5652
Zdenek Kostal <kostal.zdenek@gmail.com>
2021-04-25 20:57:43 +0200

    Do not load FZF asynchronously to fix command aliasing

--- .config/nvim/lua/plugins/misc/fzf.lua
+++ .config/nvim/lua/plugins/misc/fzf.lua
@@ -1,16 +1,13 @@
 return {
   'junegunn/fzf.vim',
   requires = {
-    {'junegunn/fzf', opt = true}
+    {'junegunn/fzf'}
   },
-  opt = true,
-  cmd = {'GitFiles', 'Files', 'Buffers', 'Marks', 'Rg', 'F'},
   setup = function()
     vim.api.nvim_set_keymap('n', '<Leader>t', ':Gitfiles<CR>', { noremap = true })
     vim.api.nvim_set_keymap('n', '<Leader>f', ':Files<CR>', { noremap = true })
     vim.api.nvim_set_keymap('n', '<Leader>b', ':Buffers<CR>', { noremap = true })
     vim.api.nvim_set_keymap('n', '<Leader>m', ':Marks<CR>', { noremap = true })
-
     vim.cmd [[command! -nargs=* F Rg <args>]]
   end,
 }
