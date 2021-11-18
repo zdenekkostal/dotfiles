215aaf7e92f64960feffa48380d447e2035534fb
Zdenek Kostal <kostal.zdenek@gmail.com>
2021-11-18 23:13:12 +0100

    Update nvim-cmp config

--- .config/nvim/lua/plugins/autocomplete/init.lua
+++ .config/nvim/lua/plugins/autocomplete/init.lua
@@ -8,13 +8,19 @@ return {
     {'saadparwaiz1/cmp_luasnip'},
   },
   config = function()
-    vim.o.completeopt = "menuone,noinsert,noselect"
+    vim.o.completeopt = "menu,menuone,noselect"
 
+    local luasnip = require('luasnip')
     local cmp = require('cmp')
 
     local check_back_space = function()
       local col = vim.fn.col '.' - 1
       return col == 0 or vim.fn.getline('.'):sub(col, col):match '%s' ~= nil
+    end
+
+    local has_words_before = function()
+      local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+      return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
     end
 
     local t = function(str)
@@ -27,7 +33,7 @@ return {
       },
       snippet = {
         expand = function(args)
-          require("luasnip").lsp_expand(args.body)
+          luasnip.lsp_expand(args.body)
         end,
       },
       mapping = {
