8fc984fbd24a8c51c522cb2f2522a2d8395c3f42
Zdenek Kostal <kostal.zdenek@gmail.com>
2021-11-30 22:45:42 +0100

    Run formatters synchronously before buffer save

--- .config/nvim/lua/plugins/lsp/null-ls.lua
+++ .config/nvim/lua/plugins/lsp/null-ls.lua
@@ -19,10 +19,7 @@ return {
     require('lspconfig')['null-ls'].setup({
       on_attach = function(client)
         if client.resolved_capabilities.document_formatting then
-          vim.api.nvim_command [[augroup Format]]
-          vim.api.nvim_command [[autocmd! * <buffer>]]
-          vim.api.nvim_command [[autocmd BufWritePost <buffer> lua vim.lsp.buf.formatting()]]
-          vim.api.nvim_command [[augroup END]]
+          vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()")
         end
       end
     })
