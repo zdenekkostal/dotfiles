c5bc6bbdd52629e8a87d81baa97e9ea392620c22
Zdenek Kostal <kostal.zdenek@gmail.com>
2021-12-14 23:19:15 +0100

    Update null_ls config

--- .config/nvim/lua/plugins/lsp/null-ls.lua
+++ .config/nvim/lua/plugins/lsp/null-ls.lua
@@ -1,27 +1,23 @@
 return {
   'jose-elias-alvarez/null-ls.nvim',
   requires = {
-    {'neovim/nvim-lspconfig'},
     {'plenary.nvim'}
   },
   config = function()
     local null_ls = require('null-ls')
 
-    null_ls.config({
+    null_ls.setup({
       sources = {
         null_ls.builtins.formatting.prettierd,
         null_ls.builtins.diagnostics.eslint_d,
         null_ls.builtins.diagnostics.shellcheck,
         null_ls.builtins.diagnostics.markdownlint,
       },
-    })
-
-    require('lspconfig')['null-ls'].setup({
       on_attach = function(client)
         if client.resolved_capabilities.document_formatting then
-          vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()")
+            vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()")
         end
-      end
+      end,
     })
   end
 }
