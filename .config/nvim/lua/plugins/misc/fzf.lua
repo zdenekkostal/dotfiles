2fa4fc63121a4135b83eb84aaa03a35d4d457899
Zdenek Kostal <kostal.zdenek@gmail.com>
2022-06-30 09:11:47 +0200

    Update fzf nvim command

--- .config/nvim/lua/plugins/misc/fzf.lua
+++ .config/nvim/lua/plugins/misc/fzf.lua
@@ -4,7 +4,7 @@ return {
     {'junegunn/fzf'}
   },
   setup = function()
-    vim.api.nvim_set_keymap('n', '<Leader>t', ':Gitfiles<CR>', { noremap = true })
+    vim.api.nvim_set_keymap('n', '<Leader>t', ':GitFiles<CR>', { noremap = true })
     vim.api.nvim_set_keymap('n', '<Leader>f', ':Files<CR>', { noremap = true })
     vim.api.nvim_set_keymap('n', '<Leader>b', ':Buffers<CR>', { noremap = true })
     vim.api.nvim_set_keymap('n', '<Leader>m', ':Marks<CR>', { noremap = true })
