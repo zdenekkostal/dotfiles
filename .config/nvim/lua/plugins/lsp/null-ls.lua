014d4f7be10ee2e29e48887d73f4fef23bf12130
Zdenek Kostal <kostal.zdenek@gmail.com>
2022-06-30 09:12:24 +0200

    Add gofmt to lsp

--- .config/nvim/lua/plugins/lsp/null-ls.lua
+++ .config/nvim/lua/plugins/lsp/null-ls.lua
@@ -9,13 +9,14 @@ return {
     null_ls.setup({
       sources = {
         null_ls.builtins.formatting.prettierd,
+        null_ls.builtins.formatting.gofmt,
         null_ls.builtins.diagnostics.eslint_d,
         null_ls.builtins.diagnostics.shellcheck,
         null_ls.builtins.diagnostics.markdownlint,
       },
       on_attach = function(client)
         if client.resolved_capabilities.document_formatting then
-            vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()")
+          vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()")
         end
       end,
     })
