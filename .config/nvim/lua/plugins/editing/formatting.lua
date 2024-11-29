530e21404a526c8f9663a2ba18a46b89fa9a696b
Zdenek Kostal <kostal.zdenek@gmail.com>
2024-11-29 15:27:10 +0100

    Setup gci

--- .config/nvim/lua/plugins/editing/formatting.lua
+++ .config/nvim/lua/plugins/editing/formatting.lua
@@ -2,9 +2,17 @@ return {
   'stevearc/conform.nvim',
   opts = {},
   config = function()
+    require("conform").formatters.gci = {
+      prepend_args = function(self, ctx)
+        local origin_url = vim.fn.systemlist("git config --get remote.origin.url")
+        local user_repo = origin_url[1]:match(":([^%.]+)%.git$")
+        return { "-s", "standard", "-s", "default", "-s", "prefix(github.com/" .. user_repo .. ")" }
+      end,
+    }
+
     require("conform").setup({
       formatters_by_ft = {
-        go = { "gofumpt", "gofmt", stop_after_first = true },
+        go = { "gofumpt", "gci" },
         python = { "ruff_fix", "ruff_format", "ruff_organize_imports" },
         rust = { "rustfmt", lsp_format = "fallback" },
         javascript = { "prettierd", "prettier", stop_after_first = true },
