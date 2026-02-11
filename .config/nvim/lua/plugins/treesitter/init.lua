6ebdd473af32af7ba3c85f2b7f403f9f62544fe4
Zdenek Kostal <kostal.zdenek@gmail.com>
2026-02-11 10:14:27 +0100

    Fix treesitter update on lazy

--- .config/nvim/lua/plugins/treesitter/init.lua
+++ .config/nvim/lua/plugins/treesitter/init.lua
@@ -1,6 +1,6 @@
 return {
   'nvim-treesitter/nvim-treesitter',
-  run = ':TSUpdate',
+  build = ':TSUpdate',
   config = function()
     require('nvim-treesitter.configs').setup({
       ensure_installed = {
