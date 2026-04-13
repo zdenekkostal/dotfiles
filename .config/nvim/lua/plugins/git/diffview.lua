b95a6ea576e7042ee7a51bdd0e4b176bcb0c9fe2
Zdenek Kostal <kostal.zdenek@gmail.com>
2026-04-13 20:05:51 +0200

    Improve Git diffing workflow in Neovim
    Reorganize diffview and gitsigns keybindings under consistent
    <leader>g and <leader>h prefixes with which-key friendly descriptions.
    
    Diffview changes:
    - Add file/line/range history with --follow for rename tracking
    - Add diff against main/master and origin branches
    - Add conflict resolution keymaps in diff view
    - Add staging shortcuts in file panel (s/S/U/X)
    
    Gitsigns changes:
    - Add visual mode support for staging/resetting hunks
    - Add inline preview and visual diff toggles (word/line/deleted)
    - Add 'ih' text object for selecting hunks
    - Add undo stage and diff against parent commit
    
    Inspired by https://naseraleisa.com/posts/diff

--- .config/nvim/lua/plugins/git/diffview.lua
+++ .config/nvim/lua/plugins/git/diffview.lua
@@ -1,76 +1,142 @@
+local function get_default_branch_name()
+  local res = vim.system({ 'git', 'rev-parse', '--verify', 'main' }, { capture_output = true }):wait()
+  return res.code == 0 and 'main' or 'master'
+end
+
 return {
   'sindrets/diffview.nvim',
+  cmd = { 'DiffviewOpen', 'DiffviewClose', 'DiffviewFileHistory' },
   keys = {
-    { '<leader>dvo', '<cmd>DiffviewOpen<cr>', desc = 'DiffviewOpen' },
-    { '<leader>dvc', '<cmd>DiffviewClose<cr>', desc = 'DiffviewClose' },
-    { '<leader>dvh', '<cmd>DiffviewFileHistory %<cr>', desc = 'DiffviewFileHistory' },
+    -- Shallow diffing (working directory changes)
+    { '<leader>gd', '<cmd>DiffviewOpen<cr>', desc = '[g]it [d]iff repo' },
+    { '<leader>gD', '<cmd>DiffviewClose<cr>', desc = '[g]it [D]iff close' },
+
+    -- Deep diffing (history)
+    { '<leader>ghh', '<cmd>DiffviewFileHistory<cr>', desc = '[g]it [h]istory repo' },
+    { '<leader>ghf', '<cmd>DiffviewFileHistory --follow %<cr>', desc = '[g]it [h]istory [f]ile' },
+    { '<leader>ghl', '<cmd>.DiffviewFileHistory --follow<cr>', desc = '[g]it [h]istory [l]ine' },
+    { '<leader>ghl', "<esc><cmd>'<,'>DiffviewFileHistory --follow<cr>", mode = 'v', desc = '[g]it [h]istory [l]ines' },
+
+    -- Diff against default branch
+    {
+      '<leader>ghm',
+      function()
+        vim.cmd('DiffviewOpen ' .. get_default_branch_name())
+      end,
+      desc = '[g]it [h]istory vs [m]ain',
+    },
+    {
+      '<leader>ghr',
+      function()
+        vim.cmd('DiffviewOpen origin/' .. get_default_branch_name())
+      end,
+      desc = '[g]it [h]istory vs [r]emote main',
+    },
   },
   opts = {
-    enhanced_diff_hl = false,
+    enhanced_diff_hl = true,
     use_icons = true,
+    view = {
+      merge_tool = {
+        layout = 'diff3_mixed',
+      },
+    },
   },
   config = function(_, opts)
+    local actions = require('diffview.actions')
+
     opts.keymaps = {
+      view = {
+        { 'n', 'q', '<cmd>DiffviewClose<cr>', { desc = 'Close diffview' } },
+        { 'n', '<leader>co', actions.conflict_choose('ours'), { desc = '[c]onflict choose [o]urs' } },
+        { 'n', '<leader>ct', actions.conflict_choose('theirs'), { desc = '[c]onflict choose [t]heirs' } },
+        { 'n', '<leader>cb', actions.conflict_choose('base'), { desc = '[c]onflict choose [b]ase' } },
+        { 'n', '<leader>ca', actions.conflict_choose('all'), { desc = '[c]onflict choose [a]ll' } },
+        { 'n', '<leader>cO', actions.conflict_choose_all('ours'), { desc = '[c]onflict all [O]urs' } },
+        { 'n', '<leader>cT', actions.conflict_choose_all('theirs'), { desc = '[c]onflict all [T]heirs' } },
+        { 'n', '<leader>cB', actions.conflict_choose_all('base'), { desc = '[c]onflict all [B]ase' } },
+        { 'n', '<leader>cA', actions.conflict_choose_all('all'), { desc = '[c]onflict all [A]ll' } },
+      },
       file_panel = {
-        { 'n', '<leader>cF', function()
-          local view = require('diffview.lib').get_current_view()
-          if not view then
-            vim.notify("No diffview open", vim.log.levels.WARN)
-            return
-          end
+        { 'n', 'q', '<cmd>DiffviewClose<cr>', { desc = 'Close diffview' } },
+        { 'n', 's', actions.toggle_stage_entry, { desc = '[s]tage/unstage entry' } },
+        { 'n', 'S', actions.stage_all, { desc = '[S]tage all' } },
+        { 'n', 'U', actions.unstage_all, { desc = '[U]nstage all' } },
+        { 'n', 'X', actions.restore_entry, { desc = 'Restore entry' } },
+        { 'n', 'R', actions.refresh_files, { desc = '[R]efresh' } },
+        { 'n', '<cr>', actions.goto_file_edit, { desc = 'Open file' } },
+        { 'n', 'o', actions.goto_file_edit, { desc = 'Open file' } },
+        { 'n', '<c-o>', actions.goto_file_split, { desc = 'Open in split' } },
+        { 'n', '<c-t>', actions.goto_file_tab, { desc = 'Open in tab' } },
+        {
+          'n',
+          '<leader>cF',
+          function()
+            local view = require('diffview.lib').get_current_view()
+            if not view then
+              vim.notify('No diffview open', vim.log.levels.WARN)
+              return
+            end
 
-          local file = view.panel.cur_file  -- Changed from cur_file() to cur_file
-          if not file then
-            vim.notify("No file selected", vim.log.levels.WARN)
-            return
-          end
+            local file = view.panel.cur_file
+            if not file then
+              vim.notify('No file selected', vim.log.levels.WARN)
+              return
+            end
 
-          local file_path = file.path
-          local left_commit = view.left.commit or "HEAD"
-          local right_commit = view.right.commit
-          
-          -- Build the git diff command
-          local cmd
-          if right_commit then
-            -- Comparing two commits
-            cmd = string.format(
-              "GIT_EXTERNAL_DIFF='difft --display side-by-side-show-both' git diff %s:%s %s:%s",
-              left_commit, file_path, right_commit, file_path
-            )
-          else
-            -- Comparing commit with working directory
-            cmd = string.format(
-              "GIT_EXTERNAL_DIFF='difft --display side-by-side-show-both' git diff %s -- %s",
-              left_commit, file_path
-            )
-          end
+            local file_path = file.path
+            local left_commit = view.left.commit or 'HEAD'
+            local right_commit = view.right.commit
 
-          -- Create floating window
-          local popup_buf = vim.api.nvim_create_buf(false, true)
-          local width = vim.o.columns - 6
-          local height = vim.o.lines - 6
-          local win_opts = {
-            focusable = true,
-            style = "minimal",
-            border = "rounded",
-            relative = "editor",
-            width = width,
-            height = height,
-            row = 3,
-            col = 3,
-            noautocmd = true,
-          }
-          local popup_win = vim.api.nvim_open_win(popup_buf, true, win_opts)
+            local cmd
+            if right_commit then
+              cmd = string.format(
+                "GIT_EXTERNAL_DIFF='difft --display side-by-side-show-both' git diff %s:%s %s:%s",
+                left_commit,
+                file_path,
+                right_commit,
+                file_path
+              )
+            else
+              cmd = string.format(
+                "GIT_EXTERNAL_DIFF='difft --display side-by-side-show-both' git diff %s -- %s",
+                left_commit,
+                file_path
+              )
+            end
 
-          vim.fn.termopen(cmd)
-          
-          -- Add keymap to close the popup
-          vim.keymap.set('n', 'q', '<cmd>close<cr>', { buffer = popup_buf, nowait = true })
-          vim.keymap.set('n', '<Esc>', '<cmd>close<cr>', { buffer = popup_buf, nowait = true })
-        end, { desc = "Diff with difftastic" } },
+            local popup_buf = vim.api.nvim_create_buf(false, true)
+            local width = vim.o.columns - 6
+            local height = vim.o.lines - 6
+            local win_opts = {
+              focusable = true,
+              style = 'minimal',
+              border = 'rounded',
+              relative = 'editor',
+              width = width,
+              height = height,
+              row = 3,
+              col = 3,
+              noautocmd = true,
+            }
+            vim.api.nvim_open_win(popup_buf, true, win_opts)
+            vim.fn.termopen(cmd)
+            vim.keymap.set('n', 'q', '<cmd>close<cr>', { buffer = popup_buf, nowait = true })
+            vim.keymap.set('n', '<Esc>', '<cmd>close<cr>', { buffer = popup_buf, nowait = true })
+          end,
+          { desc = 'Diff with difftastic' },
+        },
+      },
+      file_history_panel = {
+        { 'n', 'q', '<cmd>DiffviewClose<cr>', { desc = 'Close diffview' } },
+        { 'n', '<cr>', actions.goto_file_edit, { desc = 'Open file' } },
+        { 'n', 'o', actions.goto_file_edit, { desc = 'Open file' } },
+        { 'n', 'y', actions.copy_hash, { desc = 'Copy commit hash' } },
+        { 'n', 'zR', actions.open_all_folds, { desc = 'Open all folds' } },
+        { 'n', 'zM', actions.close_all_folds, { desc = 'Close all folds' } },
       },
     }
-    
+
     require('diffview').setup(opts)
   end,
 }
--- .config/nvim/lua/plugins/git/signs.lua
+++ .config/nvim/lua/plugins/git/signs.lua
@@ -1,23 +1,42 @@
 return {
   'lewis6991/gitsigns.nvim',
   event = { 'BufReadPre', 'BufNewFile' },
-  config = function()
-    require('gitsigns').setup({
-      current_line_blame_opts = {
-        delay = 300,
-      },
-    })
-  end,
+  opts = {
+    current_line_blame_opts = {
+      delay = 300,
+    },
+  },
   keys = {
-    { '<leader>gb', '<cmd>Gitsigns blame_line<cr>', desc = '[g]it [b]lame line' },
-    { '<leader>gB', '<cmd>Gitsigns toggle_current_line_blame<cr>', desc = '[g]it toggle inline [B]lame' },
+    -- Navigation
+    { ']h', '<cmd>Gitsigns next_hunk<cr>', desc = 'Next hunk' },
+    { '[h', '<cmd>Gitsigns prev_hunk<cr>', desc = 'Prev hunk' },
+
+    -- Hunk actions
     { '<leader>hs', '<cmd>Gitsigns stage_hunk<cr>', desc = '[h]unk [s]tage' },
+    { '<leader>hs', ':Gitsigns stage_hunk<cr>', mode = 'v', desc = '[h]unk [s]tage' },
     { '<leader>hr', '<cmd>Gitsigns reset_hunk<cr>', desc = '[h]unk [r]eset' },
+    { '<leader>hr', ':Gitsigns reset_hunk<cr>', mode = 'v', desc = '[h]unk [r]eset' },
+    { '<leader>hu', '<cmd>Gitsigns undo_stage_hunk<cr>', desc = '[h]unk [u]ndo stage' },
     { '<leader>hS', '<cmd>Gitsigns stage_buffer<cr>', desc = '[h]unk [S]tage buffer' },
     { '<leader>hR', '<cmd>Gitsigns reset_buffer<cr>', desc = '[h]unk [R]eset buffer' },
     { '<leader>hp', '<cmd>Gitsigns preview_hunk<cr>', desc = '[h]unk [p]review' },
+    { '<leader>hP', '<cmd>Gitsigns preview_hunk_inline<cr>', desc = '[h]unk [P]review inline' },
+
+    -- Diff this
     { '<leader>hd', '<cmd>Gitsigns diffthis<cr>', desc = '[h]unk [d]iff this' },
-    { ']h', '<cmd>Gitsigns next_hunk<cr>', desc = 'Next hunk' },
-    { '[h', '<cmd>Gitsigns prev_hunk<cr>', desc = 'Prev hunk' },
+    { '<leader>hD', function() require('gitsigns').diffthis('~') end, desc = '[h]unk [D]iff against ~' },
+
+    -- Blame
+    { '<leader>gb', '<cmd>Gitsigns blame_line<cr>', desc = '[g]it [b]lame line' },
+    { '<leader>gB', '<cmd>Gitsigns toggle_current_line_blame<cr>', desc = '[g]it toggle inline [B]lame' },
+
+    -- Visual toggles (in-buffer visualization)
+    { '<leader>hvw', '<cmd>Gitsigns toggle_word_diff<cr>', desc = '[h]unk [v]iew [w]ord diff' },
+    { '<leader>hvl', '<cmd>Gitsigns toggle_linehl<cr>', desc = '[h]unk [v]iew [l]ine highlight' },
+    { '<leader>hvd', '<cmd>Gitsigns toggle_deleted<cr>', desc = '[h]unk [v]iew [d]eleted' },
+    { '<leader>hvs', '<cmd>Gitsigns toggle_signs<cr>', desc = '[h]unk [v]iew [s]igns' },
+
+    -- Text object for hunks (use in visual/operator mode)
+    { 'ih', ':<c-u>Gitsigns select_hunk<cr>', mode = { 'o', 'x' }, desc = 'inner hunk' },
   },
 }
