dc1b993064e43a37d604c389362f8f15c1a4b4e1
Zdenek Kostal <kostal.zdenek@gmail.com>
2026-04-15 20:31:07 +0200

    Add gruvbox overrides for treesitter and fzf-lua
    Add treesitter highlight overrides to give Go (and other languages)
    richer syntax coloring:
    
    - @variable.member/@property → GruvboxAqua (struct fields)
    - @variable.parameter → GruvboxBlue (function parameters)
    - @function.call/@function.method.call → GruvboxGreenBold
    - @type/@constructor → GruvboxYellow, @type.builtin → bold
    
    Add fzf-lua highlight groups linked to NormalFloat/FloatBorder so
    the picker inherits gruvbox colors in both light and dark modes.

--- .config/nvim/lua/plugins/themes/gruvbox.lua
+++ .config/nvim/lua/plugins/themes/gruvbox.lua
@@ -6,6 +6,33 @@ return {
     require('gruvbox').setup({
       contrast = 'medium',
       transparent_mode = false,
+      overrides = {
+        -- Treesitter: give variables and modules some color
+        ["@variable"]           = { link = "GruvboxFg1" },
+        ["@variable.member"]    = { link = "GruvboxAqua" },
+        ["@variable.parameter"] = { link = "GruvboxBlue" },
+        ["@module"]             = { link = "GruvboxAqua" },
+        ["@property"]           = { link = "GruvboxAqua" },
+        ["@function.call"]      = { link = "GruvboxGreenBold" },
+        ["@function.method.call"] = { link = "GruvboxGreenBold" },
+        ["@type"]               = { link = "GruvboxYellow" },
+        ["@type.builtin"]       = { link = "GruvboxYellowBold" },
+        ["@constructor"]        = { link = "GruvboxYellow" },
+
+        -- fzf-lua picker: solid gruvbox background instead of white
+        FzfLuaNormal        = { link = "NormalFloat" },
+        FzfLuaBorder        = { link = "FloatBorder" },
+        FzfLuaTitle         = { link = "FloatTitle" },
+        FzfLuaPreviewNormal = { link = "NormalFloat" },
+        FzfLuaPreviewBorder = { link = "FloatBorder" },
+        FzfLuaPreviewTitle  = { link = "FloatTitle" },
+        FzfLuaCursorLine    = { link = "CursorLine" },
+        FzfLuaSearch        = { link = "IncSearch" },
+        FzfLuaHeaderText    = { link = "Comment" },
+        FzfLuaFzfNormal     = { link = "NormalFloat" },
+        FzfLuaFzfCursorLine = { link = "CursorLine" },
+        FzfLuaFzfMatch      = { link = "Special" },
+      },
     })
   end,
 }
